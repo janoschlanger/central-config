1df5f52d3b3c8ca7fb8882f8560543ce776eb7bc
Janosch Langer <janosch.langer@cgi.com>
2017-06-06 13:14:12 -0600

    Initial version generated from spring.io.

---
+++ src/main/java/j/la/centralconfig/CentralConfigApplication.java
@@ -0,0 +1,12 @@
+package j.la.centralconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralConfigApplication.class, args);
+	}
+}
